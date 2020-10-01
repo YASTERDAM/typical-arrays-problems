cf5569b1a2e4845d9fc2d216b88ff8b5fe252751
Ivan Chelyadinskiy <ivan.drozd26062606@gmail.com>
2020-10-02 00:56:02 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!(array instanceof Array)) return 0;
+    if (array.length !== 0) return Math.min(...array)
+    else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!(array instanceof Array)) return 0;
+    if (array.length !== 0) return Math.max(...array)
+    else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!(array instanceof Array)) return 0;
+    if (array.length !== 0) {
+        let res = 0;
+        for (let i in array) {
+            res += array[i];
+        }
+        return res / array.length;
+    } else return 0;
 }
